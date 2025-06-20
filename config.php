f7ab1e4eeea37a96689ac90103f30d52404183a2
Diego Armond <diego.armond@live.com>
2025-06-20 14:31:52 -0300

    config conexão

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ require_once __DIR__ . '/load_env.php';
 return [
     'database' => [
         'driver' => 'mysql',
-        'host' => getenv('DB_HOST') ?: 'localhost',
-        'dbname' => getenv('DB_NAME') ?: 'database',
-        'username' => getenv('DB_USER') ?: 'user',
-        'password' => getenv('DB_PASS') ?: '',
+        'host' => getenv('DB_HOST') ?: 'chatbot_mysql',
+        'dbname' => getenv('DB_NAME') ?: 'deliverypro',
+        'username' => getenv('DB_USER') ?: 'deliverypro',
+        'password' => getenv('DB_PASS') ?: 'C@104rm0nd1994',
         'charset' => 'utf8mb4'
     ],
     'debug' => true,
--- database/db.php
+++ database/db.php
@@ -4,10 +4,10 @@ date_default_timezone_set('America/Sao_Paulo');
 
 require_once __DIR__ . '/../load_env.php';
 
-$servername = getenv('DB_HOST') ?: 'localhost';
-$username   = getenv('DB_USER') ?: 'user';
-$password   = getenv('DB_PASS') ?: '';
-$dbname     = getenv('DB_NAME') ?: 'database';
+$servername = getenv('DB_HOST') ?: 'chatbot_mysql';
+$username   = getenv('DB_USER') ?: 'deliverypro';
+$password   = getenv('DB_PASS') ?: 'C@104rm0nd1994';
+$dbname     = getenv('DB_NAME') ?: 'deliverypro';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
